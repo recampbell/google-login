c03e64f3d5b5d8f4780e4d08ec70cd8752521e42
Ryan Campbell <ryan.campbell@gmail.com>
2014-11-29 21:54:36 -0600

    Initial checkin

---
+++ src/main/java/org/jenkinsci/plugins/oauthlogin/GoogleOAuth2SecurityRealm.java
@@ -0,0 +1,245 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.plugins.oauthlogin;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import hudson.Extension;
+import hudson.Util;
+import hudson.model.Descriptor;
+import hudson.model.User;
+import hudson.security.SecurityRealm;
+import hudson.util.HttpResponses;
+import hudson.util.Secret;
+import jenkins.model.Jenkins;
+import org.acegisecurity.Authentication;
+import org.acegisecurity.AuthenticationException;
+import org.acegisecurity.AuthenticationManager;
+import org.acegisecurity.BadCredentialsException;
+import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
+import org.acegisecurity.providers.anonymous.AnonymousAuthenticationToken;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.Header;
+import org.kohsuke.stapler.HttpRedirect;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Login with Google using OpenID Connect / OAuth 2
+ *
+ */
+public class GoogleOAuth2SecurityRealm extends SecurityRealm {
+
+    /**
+     * OAuth 2 scope. This is enough to call a variety of userinfo api's.
+     */
+    private static final String SCOPE = "profile email";
+
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+    private static final GenericUrl TOKEN_SERVER_URL = new GenericUrl("https://accounts.google.com/o/oauth2/token");
+    private static final String AUTHORIZATION_SERVER_URL = "https://accounts.google.com/o/oauth2/auth";
+
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    /**
+     * The clientID from the Google Developer console.
+     */
+    private final String clientId;
+    /**
+     * The client secret from the Google Developer console.
+     */
+    private final Secret clientSecret;
+
+    /**
+     * If this is non-null, access will be restricted to this domain.
+     */
+    private final String domain;
+
+    @DataBoundConstructor
+    public GoogleOAuth2SecurityRealm(String clientId, String clientSecret, String domain) throws IOException {
+        this.clientId = clientId;
+        this.clientSecret = Secret.fromString(clientSecret);
+        this.domain = Util.fixEmpty(domain);
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public Secret getClientSecret() {
+        return clientSecret;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    /**
+     * Login begins with our {@link #doCommenceLogin(String,String)} method.
+     */
+    @Override
+    public String getLoginUrl() {
+        return "securityRealm/commenceLogin";
+    }
+
+    /**
+     * Acegi has this notion that first an {@link org.acegisecurity.Authentication} object is created
+     * by collecting user information and then the act of authentication is done
+     * later (by {@link org.acegisecurity.AuthenticationManager}) to verify it. But in case of OpenID,
+     * we create an {@link org.acegisecurity.Authentication} only after we verified the user identity,
+     * so {@link org.acegisecurity.AuthenticationManager} becomes no-op.
+     */
+    @Override
+    public SecurityComponents createSecurityComponents() {
+        return new SecurityComponents(
+                new AuthenticationManager() {
+                    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                        if (authentication instanceof AnonymousAuthenticationToken)
+                            return authentication;
+                        throw new BadCredentialsException("Unexpected authentication type: " + authentication);
+                    }
+                }
+        );
+    }
+
+    /**
+     * The login process starts from here.
+     */
+    public HttpResponse doCommenceLogin(@QueryParameter String from,  @Header("Referer") final String referer) throws IOException {
+        final String redirectOnFinish;
+        if (from != null) {
+            redirectOnFinish = from;
+        } else if (referer != null) {
+            redirectOnFinish = referer;
+        } else {
+            redirectOnFinish = Jenkins.getInstance().getRootUrl();
+        }
+
+        final AuthorizationCodeFlow flow = new AuthorizationCodeFlow.Builder(
+                BearerToken.queryParameterAccessMethod(), HTTP_TRANSPORT, JSON_FACTORY, TOKEN_SERVER_URL,
+                new ClientParametersAuthentication(clientId, clientSecret.getPlainText()), clientId, AUTHORIZATION_SERVER_URL)
+                .setScopes(Arrays.asList(SCOPE))
+                .build();
+
+        return new OAuthSession(flow,from, buildOAuthRedirectUrl(), domain) {
+            @Override
+            public HttpResponse onSuccess(String authorizationCode) {
+                try {
+                    TokenResponse response = flow.newTokenRequest(authorizationCode).setRedirectUri(buildOAuthRedirectUrl()).execute();
+                    final Credential credential = flow.createAndStoreCredential(response, null);
+
+                    HttpRequestFactory requestFactory =
+                            HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
+                                public void initialize(HttpRequest request) throws IOException {
+                                    credential.initialize(request);
+                                    request.setParser(new JsonObjectParser(JSON_FACTORY));
+                                }
+                            });
+                    GenericUrl url = new GenericUrl("https://www.googleapis.com/userinfo/v2/me");
+
+                    HttpRequest request = requestFactory.buildGetRequest(url);
+
+                    GoogleUserInfo info = request.execute().parseAs(GoogleUserInfo.class);
+                    // logs this user in.
+                    UsernamePasswordAuthenticationToken token =
+                            new UsernamePasswordAuthenticationToken(info.getEmail(), "", new GrantedAuthority[]{});
+                    SecurityContextHolder.getContext().setAuthentication(token);
+                    // update the user profile.
+                    User u = User.get(token.getName());
+                    info.updateProfile(u);
+                    return new HttpRedirect(redirectOnFinish);
+
+                } catch (IOException e) {
+                    return HttpResponses.error(500,e);
+                }
+
+            }
+        }.doCommenceLogin();
+    }
+
+    private String buildOAuthRedirectUrl() {
+        String rootUrl = Jenkins.getInstance().getRootUrl();
+        if (rootUrl == null) {
+            throw new NullPointerException("Jenkins root url should not be null");
+        } else {
+            return rootUrl + "securityRealm/finishLogin";
+        }
+    }
+
+
+    /**
+     * This is where the user comes back to at the end of the OpenID redirect ping-pong.
+     */
+    public HttpResponse doFinishLogin(StaplerRequest request) throws IOException {
+        return OAuthSession.getCurrent().doFinishLogin(request);
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends Descriptor<SecurityRealm> {
+        public String getDisplayName() {
+            return "Login with Google";
+        }
+        /*
+         TODO: Find some way to validate the credentials.
+         This current returns "Invalid OAuth 2 grant type: CLIENT_CREDENTIALS"
+        public FormValidation doCheckApiSecret(@QueryParameter String clientId, @QueryParameter String value) {
+            if (clientId == null) {
+                return FormValidation.error("API Key is required");
+            }
+            ClientCredentialsTokenRequest tokenRequest = new ClientCredentialsTokenRequest(HTTP_TRANSPORT, JSON_FACTORY, TOKEN_SERVER_URL)
+                    .setClientAuthentication(new ClientParametersAuthentication(clientId, value))
+                    .setScopes(Collections.singleton(SCOPE));
+            try {
+                TokenResponse response = tokenRequest.execute();
+                return FormValidation.ok("Credentials are valid");
+            } catch (IOException e) {
+                return FormValidation.error(e,"Credentials are invalid, or do not have expected scopes.");
+            }
+        }
+            */
+
+    }
+}
---
+++ src/main/java/org/jenkinsci/plugins/oauthlogin/GoogleUserInfo.java
@@ -0,0 +1,107 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.plugins.oauthlogin;
+
+import com.google.api.client.util.Key;
+import hudson.Extension;
+import hudson.model.User;
+import hudson.model.UserProperty;
+import hudson.model.UserPropertyDescriptor;
+import hudson.tasks.Mailer;
+
+import java.io.IOException;
+
+/**
+ * Represents an identity information from the oauth provider.
+ *
+ * This is from https://www.googleapis.com/userinfo/v2/me
+ */
+public class GoogleUserInfo extends UserProperty {
+
+    @Key
+    public String family_name;
+
+    @Key
+    public String name;
+
+    @Key
+    public String picture;
+
+    @Key
+    public String locale;
+
+    @Key
+    public String gender;
+
+    @Key
+    public String email;
+
+    @Key
+    public String link;
+
+    @Key
+    public String given_name;
+
+    @Key
+    public String id;
+
+    @Key
+    public boolean verified_email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Updates the user information on Hudson based on the information in this identity.
+     */
+    public void updateProfile(hudson.model.User u) throws IOException {
+        // update the user profile by the externally given information
+        if (email != null)
+            u.addProperty(new Mailer.UserProperty(email));
+
+        if (name != null)
+            u.setFullName(name);
+
+        u.addProperty(this);
+    }
+
+    @Extension
+    public static class DescriptorImpl extends UserPropertyDescriptor {
+
+        @Override
+        public UserProperty newInstance(User user) {
+            return null;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Google Profile Info";
+        }
+    }
+}
